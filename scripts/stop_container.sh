e9e4448b0c83832aaa241eb286fa421f8d2a5f61
Saba Ramzan <sabaramzan0226@gmail.com>
2025-08-25 15:44:28 +0500

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Stop and remove any container running on port 5000
+CONTAINER_ID=$(docker ps -q --filter "publish=5000")
+if [ ! -z "$CONTAINER_ID" ]; then
+    echo "Stopping container $CONTAINER_ID..."
+    docker stop $CONTAINER_ID
+    docker rm $CONTAINER_ID
+fi
